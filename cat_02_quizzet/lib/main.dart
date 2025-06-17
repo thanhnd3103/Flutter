6d607ad6eed55efc468dbffbb9e4871c4e366cae
Thanh Nguyen <thanh.nguyen@ugotechs.com>
2025-06-17 22:34:18 +0700

    can't live without trailing comma lul

--- cat_02_quizzet/lib/main.dart
+++ cat_02_quizzet/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: Text("Hello World"),),),);
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Text("Hello World"),
+      ),
+    ),
+  );
 }
