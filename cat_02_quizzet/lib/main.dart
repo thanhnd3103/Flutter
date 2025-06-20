8fa6cc123bb157a6b27934fbcafd9d0eedf20981
Thanh Nguyen <thanh.nguyen@ugotechs.com>
2025-06-20 14:20:20 +0700

    refactor start screen

--- cat_02_quizzet/lib/main.dart
+++ cat_02_quizzet/lib/main.dart
@@ -3,9 +3,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: WelcomeContainer(),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.blue.shade200,
+                Colors.blue.shade400,
+                Colors.blue.shade600,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const WelcomeContainer(),
+        ),
       ),
     ),
   );
