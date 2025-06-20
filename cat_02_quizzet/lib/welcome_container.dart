33a2df2065771aabe99b7c02f681d617579f70a0
Thanh Nguyen <thanh.nguyen@ugotechs.com>
2025-06-20 14:51:45 +0700

    change to outline button, add opacity (2 ways of adding)

--- cat_02_quizzet/lib/welcome_container.dart
+++ cat_02_quizzet/lib/welcome_container.dart
@@ -7,14 +7,26 @@ class WelcomeContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Column(
-        mainAxisSize: MainAxisSize.max,
+        mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          //========================================Use lots of resources========================================
+          // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 400,
+          //     height: 400,
+          //   ),
+          // ),
+          //=====================================================================================================
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 400,
             height: 400,
+            color: Colors.white.withAlpha(220)
           ),
+          const SizedBox(height: 20),
           const Text(
             "Welcome to the Quiz App!",
             style: TextStyle(
@@ -23,9 +35,16 @@ class WelcomeContainer extends StatelessWidget {
               color: Colors.white,
             ),
           ),
-          const ElevatedButton(
-            onPressed: null,
-            child: Text("Start Quiz"),
+          const SizedBox(height: 10),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              side: const BorderSide(color: Colors.white),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            ),
+            icon: const Icon(Icons.play_arrow),
+            label: const Text("Start Quiz"),
           ),
         ],
       ),
