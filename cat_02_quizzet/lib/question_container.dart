4544860fa142e463b3230e9cecf80af39e692b0e
Thanh Nguyen <thanh.nguyen@ugotechs.com>
2025-06-24 09:42:38 +0700

    dynamic question

--- cat_02_quizzet/lib/question_container.dart
+++ cat_02_quizzet/lib/question_container.dart
@@ -1,5 +1,6 @@
 import 'package:cat_02_quizzet/answer_button.dart';
 import 'package:flutter/material.dart';
+import 'package:cat_02_quizzet/data/question_list.dart';
 
 class QuestionContainer extends StatefulWidget {
   const QuestionContainer({super.key});
@@ -11,29 +12,29 @@ class QuestionContainer extends StatefulWidget {
 class _QuestionContainerState extends State<QuestionContainer> {
   @override
   Widget build(BuildContext context) {
+    final questions = questionList;
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question will go here',
-            style: TextStyle(fontSize: 24, color: Colors.white),
+          Text(
+            questions[0].text,
+            style: const TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
           ),
           const SizedBox(height: 20),
-          
-          AnswerButton(
-            answerText: 'Answer 1',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'Answer 2',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'Answer 3',
-            onTap: () {},
-          ),
+          for (var i = 0; i < questions[0].answers.length; i++)
+            AnswerButton(
+              answerText: questions[0].answers[i],
+              onTap: () {
+                
+              },
+            ),
         ],
       ),
     );
