d95451258721f63f502a09cbcc79c7d463ef33fd
Thanh Nguyen <thanh.nguyen@ugotechs.com>
2025-06-30 21:06:10 +0700

    UI: Add more UI Element

--- cat_03_expense_tracker/lib/widgets/modals/add_expense.dart
+++ cat_03_expense_tracker/lib/widgets/modals/add_expense.dart
@@ -11,12 +11,22 @@ class AddExpense extends StatefulWidget {
 }
 
 class _AddExpenseState extends State<AddExpense> {
-
   //=======================================
   //You must tell flutter to delete the controller
   //when the widget is disposed to avoid memory leaks
   //=======================================
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    //========================================
+    // Always call super.dispose() to ensure proper cleanup
+    //========================================
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,25 +35,52 @@ class _AddExpenseState extends State<AddExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _onTitleChanged,
+            controller: _titleController,
             maxLength: 50,
             decoration: const InputDecoration(
               label: Text("Title"),
               border: OutlineInputBorder(),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              label: Text("Amount"),
+              prefixText: '\$',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 10),
           Row(
             children: [
               const Spacer(),
               TextButton.icon(
+                onPressed: () {},
+                label: const Text("Cancel"),
+                icon: const Icon(Icons.cancel),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green.shade100,
+                  foregroundColor: Colors.green.shade900,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
+                  ),
+                  textStyle: GoogleFonts.montserrat(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton.icon(
                 onPressed: () {
-                  print(_enteredTitle);
+                  
                 },
                 label: const Text("Save"),
                 icon: const Icon(Icons.save_as),
                 style: TextButton.styleFrom(
-                  backgroundColor: Colors.green.shade50,
-                  foregroundColor: Colors.green.shade900,
+                  backgroundColor: Colors.green.shade900,
+                  foregroundColor: Colors.green.shade50,
                   padding: const EdgeInsets.symmetric(
                     horizontal: 20,
                     vertical: 10,
