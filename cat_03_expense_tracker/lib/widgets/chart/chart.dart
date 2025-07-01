ce127ba548e8dc21c234e1fdf8e9bad3ecee5e50
Thanh Nguyen <thanh.nguyen@ugotechs.com>
2025-07-01 20:49:46 +0700

    UI: Copy UI

---
+++ cat_03_expense_tracker/lib/widgets/chart/chart.dart
@@ -0,0 +1,101 @@
+import 'package:cat_03_expense_tracker/_models/expense.dart';
+import 'package:cat_03_expense_tracker/_models/expense_bucket.dart';
+import 'package:cat_03_expense_tracker/_utils/custom_enum.dart';
+import 'package:cat_03_expense_tracker/widgets/chart/chart_bar.dart';
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  List<ExpenseBucket> get buckets {
+    return [
+      ExpenseBucket.forCategory(allExpenses: expenses, category: Category.food),
+      ExpenseBucket.forCategory(
+        allExpenses: expenses,
+        category: Category.leisure,
+      ),
+      ExpenseBucket.forCategory(
+        allExpenses: expenses,
+        category: Category.travel,
+      ),
+      ExpenseBucket.forCategory(allExpenses: expenses, category: Category.work),
+    ];
+  }
+
+  double get maxTotalExpense {
+    double maxTotalExpense = 0;
+
+    for (final bucket in buckets) {
+      if (bucket.totalExpenses > maxTotalExpense) {
+        maxTotalExpense = bucket.totalExpenses;
+      }
+    }
+
+    return maxTotalExpense;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    return Container(
+      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 8,
+      ),
+      width: double.infinity,
+      height: 180,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        gradient: LinearGradient(
+          colors: [
+            Theme.of(context).colorScheme.primary.withValues(alpha: 0.3),
+            Theme.of(context).colorScheme.primary.withValues(alpha: 0.0),
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                for (final bucket in buckets) // alternative to map()
+                  ChartBar(
+                    fill: bucket.totalExpenses == 0
+                        ? 0
+                        : bucket.totalExpenses / maxTotalExpense,
+                  ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 12),
+          Row(
+            children: buckets
+                .map(
+                  (bucket) => Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
+                      child: Icon(
+                        categoryIcons[bucket.category],
+                        color: isDarkMode
+                            ? Theme.of(context).colorScheme.secondary
+                            : Theme.of(
+                                context,
+                              ).colorScheme.primary.withValues(alpha: 0.7),
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
