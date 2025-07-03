55cde9a2d23c725a84a297acffccc3245023e884
Thanh Nguyen <thanh.nguyen@ugotechs.com>
2025-07-03 22:58:48 +0700

    UI: Add MealScreen with ListView

--- cat_05_mew_meal/lib/screens/meals_screen.dart
+++ cat_05_mew_meal/lib/screens/meals_screen.dart
@@ -1,11 +1,49 @@
+import 'package:cat_05_mew_meal/_models/meal.dart';
 import 'package:flutter/material.dart';
 
-class MealsScreen extends StatelessWidget{
-  const MealsScreen({super.key});
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.mealList});
+
+  final String title;
+  final List<Meal> mealList;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    Widget content = ListView.builder(
+      itemCount: mealList.length,
+      itemBuilder: (ctx, index) => Text(mealList[index].title),
+    );
+
+    if (mealList.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Jumpscare Mew Mew",
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              "Get out!!!!",
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: content,
+    );
   }
 }
