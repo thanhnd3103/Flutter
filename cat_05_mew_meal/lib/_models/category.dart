f5d44eb4ce570492fd497227cd1dcb3112c422a7
Thanh Nguyen <thanh.nguyen@ugotechs.com>
2025-07-03 22:39:51 +0700

    REFACTOR: Models

--- cat_05_mew_meal/lib/_models/category.dart
+++ cat_05_mew_meal/lib/_models/category.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:uuid/uuid.dart';
-
-const uuid = Uuid();
 
 class Category {
   final String id;
   final String title;
   final Color color;
 
-  Category({required this.title, this.color = Colors.orange}) : id = uuid.v8();
+  const Category({required this.id, required this.title, this.color = Colors.orange});
 }
---
+++ cat_05_mew_meal/lib/screens/meals_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MealsScreen extends StatelessWidget{
+  const MealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
