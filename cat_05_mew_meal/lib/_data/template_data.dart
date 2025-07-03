2613f7c3424236e90ec8e4efe5f648ba2393735e
Thanh Nguyen <thanh.nguyen@ugotechs.com>
2025-07-03 16:16:21 +0700

    UI: Fix a bit

---
+++ cat_05_mew_meal/lib/_data/template_data.dart
@@ -0,0 +1,45 @@
+import 'package:cat_05_mew_meal/_models/category.dart';
+import 'package:flutter/material.dart';
+
+final availableCategories = [
+  Category(
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    title: 'German',
+    color: Colors.amber,
+  ),
+  Category(
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  Category(
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Category(
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Category(
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+];
--- cat_05_mew_meal/lib/_models/category.dart
+++ cat_05_mew_meal/lib/_models/category.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
 
 class Category {
   final String id;
   final String title;
   final Color color;
 
-  Category({required this.id, required this.title, this.color = Colors.orange});
+  Category({required this.title, this.color = Colors.orange}) : id = uuid.v8();
 }
